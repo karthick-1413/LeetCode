de07b55db3e001a7321b76699a1b9a4b3354a0fd
karthick-1413 <155943932+karthick-1413@users.noreply.github.com>
2024-03-30 16:06:15 +0530

    Time: 56 ms (67.22%), Space: 50.9 MB (35.91%) - LeetHub

--- 0202-happy-number/0202-happy-number.js
+++ 0202-happy-number/0202-happy-number.js
@@ -4,52 +4,40 @@
  */
 
 var set = new Set();
+var final = 0;
 
 
 var isHappy = function (n) {
-     let number = n.toString()
-    
-    let seen = new Set();
-    
-    while(!seen.has(number)){
-        let result = 0;
-
-        for(let i = 0; i < number.length; i++){
-            result += Math.pow(parseInt(number[i]), 2)
-        }
-
-        if(result == 1) return true
-            
-        seen.add(number)
-        
-        number = result.toString();
-    }
-
-    return false;
+    set = new Set();
+    return calculate(n);
 
 };
+
+
 function calculate(n) {
-    let final = 0;
+    final =0;
 
     if (n === 1) {
         return true;
-    } else {
-        while (n > 0) {
-            let mod = n % 10;
-            y = (mod * mod);
-            final += y;
-            n = parseInt(n / 10);
-        }
-        if (final === 1) {
-            return true;
-        }
-        if (!set.has(final)) {
-            set.add(final);
-        }
-        else {
-            return false;
-        }
-        return calculate(final);
     }
-
+    while (n > 0) {
+        let mod = n % 10;
+        y = (mod * mod);
+        final += y;
+       
+        n = parseInt(n / 10);
+        
+    }
+    if (final === 1) {
+        return true;
+    } else if ( final === 0) {
+        return false;
+    }
+    if ( !set.has(final)) {
+        set.add(final);
+    }
+    else {
+        return false;
+    }
+    return calculate(final);
 }
