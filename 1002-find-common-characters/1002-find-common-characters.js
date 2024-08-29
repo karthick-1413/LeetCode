a8c82ce270bcb99f14100bcdf32d96cc4690108a
karthick-1413 <155943932+karthick-1413@users.noreply.github.com>
2024-08-29 23:11:45 +0530

    Time: 54 ms (89.21%), Space: 52.7 MB (61.15%) - LeetHub

---
+++ 1002-find-common-characters/1002-find-common-characters.js
@@ -0,0 +1,20 @@
+/**
+ * @param {string[]} words
+ * @return {string[]}
+ */
+var commonChars = function (words) {
+    let first = words[0];
+    for (let i = 1; i < words.length; i++) {
+        const word = words[i]
+        let temp = "";
+        word.split("").forEach((x) => {
+            if (first.includes(x)) {
+                temp += x;
+                const index = first.indexOf(x);
+                first = first.replace(x, "");
+            }
+        })
+        first = temp
+    }
+    return first.split("");
+}
